199720f85b309c33989d1de30be0d81d9106d1e5
lucaswatanuki <lucaswatanuki@gmail.com>
2021-04-07 23:31:35 -0300

    Correção de templates + add regencia request

---
+++ src/main/java/com/ftunicamp/tcc/repositories/RegenciaRepository.java
@@ -0,0 +1,8 @@
+package com.ftunicamp.tcc.repositories;
+
+import com.ftunicamp.tcc.model.RegenciaEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegenciaRepository extends AtividadeRepository<RegenciaEntity>{
+}
--- src/main/java/com/ftunicamp/tcc/service/PdfService.java
+++ src/main/java/com/ftunicamp/tcc/service/PdfService.java
@@ -1,13 +1,12 @@
 package com.ftunicamp.tcc.service;
 
-import com.ftunicamp.tcc.utils.TipoPDF;
-import org.thymeleaf.context.Context;
+import com.ftunicamp.tcc.model.TipoAtividade;
 
 import java.io.File;
 import java.io.IOException;
 
 public interface PdfService {
-    File generatePdf(TipoPDF tipo, long idAtividade) throws IOException;
+    File generatePdf(TipoAtividade tipo, long idAtividade) throws IOException;
 
     File renderPdf(String html) throws IOException;
 }

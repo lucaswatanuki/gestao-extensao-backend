17c14bced782cb60f927b34a7a2e580209a29181
lucaswatanuki <lucaswatanuki@gmail.com>
2021-05-12 17:13:08 -0300

    Update pom

--- src/main/java/com/ftunicamp/tcc/controllers/request/ConvenioRequest.java
+++ src/main/java/com/ftunicamp/tcc/controllers/request/ConvenioRequest.java
@@ -1,15 +1,19 @@
 package com.ftunicamp.tcc.controllers.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ftunicamp.tcc.dto.AlocacaoDto;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-public class ConvenioRequest {
+@Data
+public class ConvenioRequest implements Serializable {
 
     private String projeto;
     private String coordenador;
@@ -23,5 +27,6 @@ public class ConvenioRequest {
     private LocalDateTime dataFim;
     private String observacao;
     private String tipoAtividadeSimultanea;
-    private List<AlocacaoDto> alocacoes;
+    @JsonProperty("alocacoes")
+    private transient List<AlocacaoDto> alocacoes;
 }
